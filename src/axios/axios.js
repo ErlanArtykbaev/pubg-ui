a93723e675b7648151a317e15b761b986d81b936
erlan artykbaev <erik.2405@mail.ru>
2021-01-14 12:48:02 +0600

    axios changes

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 //third from localhost
 //fourth to test arena with url
 export default axios.create({
-  baseURL: 'http://195.38.164.24:8080'
+	baseURL: 'https://api.arenagames.io'
   //baseURL: 'http://193.124.206.13:8080'
   //baseURL: 'localhost:8080'
   //baseURL: 'https://arenagames.io:8080'
